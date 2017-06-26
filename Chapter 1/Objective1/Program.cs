e7e4672a0e0267370026d9f1271a24db67682d7e
Bruno Castro <bruno.castro@saiphernet.local>
2017-06-26 10:29:13 -0300

    Criação do projeto (Objective 1)

---
+++ Chapter 1/Objective1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Objective1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Task[] tasks = new Task[3];
+            tasks[0] = Task<int>.Run(() =>
+            {
+                Thread.Sleep(4000);
+                Console.WriteLine("1");
+                return 1;
+            });
+
+            tasks[1] = Task<int>.Run(() =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("2");
+                return 2;
+            });
+
+            tasks[2] = Task<int>.Run(() =>
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("3");
+                return 3;
+            });
+
+            Task.WaitAny(tasks);
+
+            Console.WriteLine($"Task ganhadora:  {tasks.Where(x => x.Status == TaskStatus.RanToCompletion).FirstOrDefault().Id}");
+
+            Console.WriteLine("Pressione qualquer tecla para finalizar");
+            Console.Read();
+        }
+    }
+}
