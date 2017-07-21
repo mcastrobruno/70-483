48ff4320a438d3dd89c4156e8d64130068d6a975
Bruno Castro <mcastrobruno@gmail.com>
2017-07-20 22:57:01 -0300

    Teste de Value Type e Reference Type

---
+++ Chapter 1/Review/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review
+{
+    public class Teste
+    {
+        public int valor;
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Value Type
+            Console.WriteLine("** VALUE TYPE **");
+            int valor = 5;
+            Console.WriteLine($"Valor antes do métodos: {valor}");
+            ValueType(valor);
+            Console.WriteLine($"Valor após multiplicação: {valor}");
+
+            //Reference Type
+            Console.WriteLine("** REFERENCE TYPE **");
+            Teste obj = new Teste();
+            obj.valor = 5;
+            Console.WriteLine($"Valor antes do métodos: {obj.valor}");
+            ReferenceType(obj);
+            Console.WriteLine($"Valor após multiplicação: {obj.valor}");
+
+            Console.Read();
+        }
+
+
+        static void ValueType(int valor)
+        {
+            valor = valor * 2;
+        }
+
+        static void ReferenceType(Teste obj)
+        {
+            obj.valor = obj.valor * 2;
+        }
+    }
+}
